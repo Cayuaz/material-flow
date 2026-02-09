09e57e14a9847d318e04ab9b92cbb9ad1b2ff68e
Cayuã Ronald Valentin Mendonça <cayuaronald7@gmail.com>
2026-02-08 20:34:36 -0400

    feat: add validation for object length in update use case

--- backend/src/useCases/UpdateMaterialUseCase.ts
+++ backend/src/useCases/UpdateMaterialUseCase.ts
@@ -5,10 +5,14 @@ import type { IMaterialRepository } from "../repositories/IMaterialRepository.js
 export class UpdateMaterialUseCase {
   constructor(private repository: IMaterialRepository) {}
 
-  async execute(id: string, productData: unknown) {
-    const { success, data } = updateMaterialSchema.safeParse(productData);
+  async execute(id: string, materialData: unknown) {
+    const { success, data } = updateMaterialSchema.safeParse(materialData);
 
     if (!success) throw new AppError("Invalid or incomplete data");
+
+    const props = Object.keys(data).length;
+
+    if (!props) throw new AppError("Invalid or incomplete data");
 
     await this.repository.updateMaterial(id, data);
 
